edf2e3b6c99fe9fc28daa0929c4550d7834bcfe9
JulesThepault1 <jules.thepault@gmail.com>
2023-10-09 14:42:36 +0200

    Question Seekbar

--- app/src/main/java/com/example/projectandroid/QuestionSeekbar.java
+++ app/src/main/java/com/example/projectandroid/QuestionSeekbar.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class test_echelle_taille extends AppCompatActivity {
+public class QuestionSeekbar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_echelle_taille);
+        setContentView(R.layout.activity_question_seekbar);
     }
 }
