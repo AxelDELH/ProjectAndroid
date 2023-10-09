830c4239b5d27e92ce476af6f735c2320d2281b9
AxelDELH <axel.delhaye01@gmail.com>
2023-10-09 14:39:48 +0200

    création des bouton en Java

--- app/src/main/java/com/example/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.projectandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button Quitter;
+    private Button Commencer;
+    private Button Result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.Quitter = findViewById(R.id.buttonQuit);
+        this.Commencer = findViewById(R.id.buttonStart);
+        this.Result = findViewById(R.id.buttonRes);
     }
 }
