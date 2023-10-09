f8d79d684e3cad91f47cf071590605b762888523
AxelDELH <axel.delhaye01@gmail.com>
2023-10-09 14:55:54 +0200

    push des noms de fichiers

--- app/src/main/java/com/example/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.projectandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String Log_TAG = "Intent Log";
     private Button Quitter;
     private Button Commencer;
     private Button Result;
+    private Spinner Choice;
+    private EditText Pseudo;
+    private boolean region;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +26,19 @@ public class MainActivity extends AppCompatActivity {
         this.Quitter = findViewById(R.id.buttonQuit);
         this.Commencer = findViewById(R.id.buttonStart);
         this.Result = findViewById(R.id.buttonRes);
+        this.Choice = findViewById(R.id.spinnerChoix);
+        this.Pseudo = findViewById(R.id.editTextPseudo);
+
+        Commencer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Créez une intention pour démarrer InfoPokemon ActivityB
+                Intent intent = new Intent(MainActivity.this, QuestionButton.class);
+                Log.d(Log_TAG, "creation d'intent");
+                startActivity(intent);
+                Log.d(Log_TAG, "Lancement du test");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projectandroid/QuestionButton.java
+++ app/src/main/java/com/example/projectandroid/QuestionButton.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TestRegion3 extends AppCompatActivity {
+public class QuestionButton extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_region3);
+        setContentView(R.layout.activity_question_button);
     }
 }
