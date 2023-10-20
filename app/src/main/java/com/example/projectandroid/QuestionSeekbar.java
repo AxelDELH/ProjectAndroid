e5246dd96b0e1a381a2876ba4164d515c1c79603
JulesThepault1 <jules.thepault@gmail.com>
2023-10-20 12:23:12 +0200

    code seekbar question fin

--- app/src/main/java/com/example/projectandroid/QuestionSeekbar.java
+++ app/src/main/java/com/example/projectandroid/QuestionSeekbar.java
@@ -13,10 +13,10 @@ import android.widget.TextView;
 
 public class QuestionSeekbar extends AppCompatActivity {
     private static final String Log_TAG = "Intent Log";
-    private SeekBar SeekBar;
+    private SeekBar SeekBar1;
     private SeekBar SeekBar2;
-    private TextView SeekBarValue1; //valeur de 1 à 10 de la 1er Seekbar
-    private TextView SeekBarValue2; //valeur de 1 à 10 de la 2eme Seekbar
+    public TextView SeekBarValue1; //valeur de 1 à 10 de la 1er Seekbar
+    public TextView SeekBarValue2; //valeur de 1 à 10 de la 2eme Seekbar
     private Button Next; //Button Suivant
     private Button Back; //Button Retour
 
@@ -24,12 +24,14 @@ public class QuestionSeekbar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_seekbar);
-        //Attribution des buttons
+
+        //Attribution des éléments de l'activité
         this.Next = findViewById(R.id.button_next_seekbar);
         this.Back = findViewById(R.id.button_back_seekbar);
-        this.SeekBar = findViewById(R.id.seek_bar);
-
+        this.SeekBar1 = findViewById(R.id.seek_bar);
         this.SeekBar2 = findViewById(R.id.seek_bar2);
+        this.SeekBarValue1 = findViewById(R.id.seek_bar_value);
+        this.SeekBarValue2 = findViewById(R.id.seek_bar_value2);
 
         //Initialisation des SharedPreferences : le code pour récupérer et stocker la valeur, vous devez initialiser SharedPreferences dans votre activité
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
@@ -37,16 +39,16 @@ public class QuestionSeekbar extends AppCompatActivity {
         //Récupération de la valeur précédente : Pour récupérer
         // la valeur stockée précédemment, ajoutez le code suivant sous l'initialisation de la SeekBar
 
-        int defaultValue = 5; // Valeur par défaut de 5 si aucune valeur n'est stockée.
-        int savedValue = sharedPreferences.getInt("userValue", defaultValue);
-        SeekBar.setProgress(savedValue);
+        //Code SeekBar n°1/Code SeekBar n°1/Code SeekBar n°1/Code SeekBar n°1/Code SeekBar n°1/Code SeekBar n°1/
+        int defaultValue1 = 5; // Valeur par défaut de 5 si aucune valeur n'est stockée.
+        int savedValue1 = sharedPreferences.getInt("userValue", defaultValue1);
+        SeekBar1.setProgress(savedValue1);
 
-        //Code SeekBar n°1
-        SeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        SeekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 // Mettez à jour le texte pour afficher la valeur sélectionnée.
-                SeekBarValue1.setText("Sur une échelle de 1 à 10, de quelle taille te considères-tu : " + progress);
+                SeekBarValue1.setText("valeur choisi: " + progress);
 
                 // Mettre à jour SharedPreferences avec la nouvelle valeur.
                 SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -69,6 +71,44 @@ public class QuestionSeekbar extends AppCompatActivity {
             }
         });
 
+        //Code SeekBar n°2/Code SeekBar n°2/Code SeekBar n°2/Code SeekBar n°2/Code SeekBar n°2/Code SeekBar n°2/
+        int defaultValue2 = 5; // Valeur par défaut de 5 si aucune valeur n'est stockée.
+        int savedValue2 = sharedPreferences.getInt("userValue", defaultValue2);
+        SeekBar1.setProgress(savedValue2);
+
+
+        SeekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Mettez à jour le texte pour afficher la valeur sélectionnée.
+                SeekBarValue2.setText("valeur choisi: " + progress);
+
+                // Mettre à jour SharedPreferences avec la nouvelle valeur.
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("userValue", progress);
+                editor.apply();
+
+                //Avec ces modifications, votre application récupérera la valeur choisie par
+                //l'utilisateur à partir des SharedPreferences et la mettra à jour à chaque fois que l'utilisateur
+                //déplacera la SeekBar. Cette valeur sera conservée même après la fermeture et la réouverture de l'application.
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Appelé lorsque l'utilisateur commence à déplacer la SeekBar.
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Appelé lorsque l'utilisateur arrête de déplacer la SeekBar.
+            }
+        });
+
+
+
+
+
+        //Code Boutons Suivant et Retour
         Next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
